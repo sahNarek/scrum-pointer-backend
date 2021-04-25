7f844f3a06fce3eb4904ea82029d517c8e3ba5d9
narek-sahakyan-vineti <narek.sahakyan@vineti.com>
2021-04-25 19:30:58 +0400

    feat:(return voterType in estimate)

--- app/graphql/types/estimate_type.rb
+++ app/graphql/types/estimate_type.rb
@@ -4,6 +4,7 @@ module Types
     field :ticket_id, ID, null: false
     field :voter_id, ID, null: false
     field :point, Integer, null: false
+    field :voter, Types::VoterType, null: false
     field :final_estimate, Boolean, null: false
   end
 end
